c8ce61822b47b64f41d987b9841df477c411aded
Martina Soliman <120523994+martinasoliman@users.noreply.github.com>
2026-04-21 10:38:14 +0200

    Add files via upload

---
+++ 02_Dim_Customer.sql
@@ -0,0 +1,13 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT cu.[CustomerID]
+      ,cu.[FirstName]
+      ,cu.[LastName]
+      ,cu.[Email]
+      ,cu.[PhoneNumber]
+	  ,g.GenderID
+	  ,g.GenderName
+      
+
+  FROM [Sales_OLTP].[dbo].[Customer] as cu
+  left join [Sales_OLTP].[dbo].Gender  as g
+  on(cu.GenderID=g.GenderID)
